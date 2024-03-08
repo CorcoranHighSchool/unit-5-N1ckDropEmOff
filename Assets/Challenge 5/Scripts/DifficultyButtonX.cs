6e3229b64e9dd101479a6f09af676608ca74a2b7
N1ckDropEmOff <142622275+N1ckDropEmOff@users.noreply.github.com>
2024-03-08 12:04:28 -0800

    Challenge 5

--- Assets/Challenge 5/Scripts/DifficultyButtonX.cs
+++ Assets/Challenge 5/Scripts/DifficultyButtonX.cs
@@ -23,7 +23,9 @@ public class DifficultyButtonX : MonoBehaviour
     void SetDifficulty()
     {
         Debug.Log(button.gameObject.name + " was clicked");
-        gameManagerX.StartGame();
+        GameManager gameManager = FindObjectOfType<GameManager>();
+        gameManagerX.StartGame(difficulty);
+        
     }
 
 
